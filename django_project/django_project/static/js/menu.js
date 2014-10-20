20112c5d6c377ef4fb8557edc6487fe2f8cf7160
Ladorne <xantos129@gmail.com>
2014-10-20 18:33:15 -0400

    js

--- django_project/django_project/static/js/menu.js
+++ django_project/django_project/static/js/menu.js
@@ -1,6 +1,44 @@
  document.addEventListener('polymer-ready', function() {
   // initial setup
   setup();
+  document.getElementById('content_pane').removeAttribute('hidden');
+});
+
+var meta;
+var transition;
+var state = {
+  opened: false
+}
+
+function getMeta() {
+      if (!meta) {
+        meta = document.createElement('core-meta');
+        meta.type = 'transition';
+      }
+      return meta;
+    }
+
+function setup() {
+      var target = document.getElementById('content_pane')
+
+      if (transition) {
+        transition.teardown(target);
+      }
+
+
+      var value = "core-transition-center";
+      transition = getMeta().byId(value);
+      transition.setup(target);
+    }
+
+function toggle1() {
+      var target = document.getElementById('content_pane')
+      state.opened = !state.opened;
+      transition.go(target, state);
+    }
+document.addEventListener('polymer-ready', function() {
+  // initial setup
+  setup();
   document.getElementById('side').removeAttribute('hidden');
 });
 
@@ -67,43 +105,4 @@ function toggle2() {
     init();
 
 })();
-    }
-
- document.addEventListener('polymer-ready', function() {
-  // initial setup
-  setup();
-  document.getElementById('content_pane').removeAttribute('hidden');
-});
-
-var meta;
-var transition;
-var state = {
-  opened: false
-}
-
-function getMeta() {
-      if (!meta) {
-        meta = document.createElement('core-meta');
-        meta.type = 'transition';
-      }
-      return meta;
-    }
-
-function setup() {
-      var target = document.getElementById('content_pane')
-
-      if (transition) {
-        transition.teardown(target);
-      }
-
-
-      var value = "core-transition-center";
-      transition = getMeta().byId(value);
-      transition.setup(target);
-    }
-
-function toggle1() {
-      var target = document.getElementById('content_pane')
-      state.opened = !state.opened;
-      transition.go(target, state);
     }
