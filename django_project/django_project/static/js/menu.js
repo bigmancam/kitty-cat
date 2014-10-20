9dd067ebe4bfb99d404e2e741cdbd935c6b9e56d
Ladorne <xantos129@gmail.com>
2014-10-20 18:07:28 -0400

    js

--- django_project/django_project/static/js/menu.js
+++ django_project/django_project/static/js/menu.js
@@ -1,19 +1,50 @@
-var YTMenu = (function() {
- 
-    function init() {
-         
-        [].slice.call( document.querySelectorAll( '.dr-menu' ) ).forEach( function( el, i ) {
+ document.addEventListener('polymer-ready', function() {
+  // initial setup
+  setup();
+  document.getElementById('side').removeAttribute('hidden');
+});
+
+var meta;
+var transition;
+var state = {
+  opened: true
+}
+
+function getMeta() {
+      if (!meta) {
+        meta = document.createElement('core-meta');
+        meta.type = 'transition';
+      }
+      return meta;
+    }
+
+function setup() {
+      var target = document.getElementById('side')
+
+      if (transition) {
+        transition.teardown(target);
+      }
+
+
+      var value = "core-transition-center";
+      transition = getMeta().byId(value);
+      transition.setup(target);
+    }
+
+function toggle2() {
+      var target = document.getElementById('side')
+      state.opened = !state.opened;
+      transition.go(target, state);
+      [].slice.call( document.querySelectorAll( '.dr-menu' ) ).forEach( function( el, i ) {
             var trigger = document.getElementById( 'dr-trigger' ),
                 icon = document.getElementById( 'label' ),
                 open = false;
- 
-            icon.addEventListener( 'click', function( event ) {
-                if( !open ) {
+            if( !open ) {
                     el.className += ' dr-menu-open';
                     open = true;
                     return;
                 }
-                if( open ) {
+            if( open ) {
                     event.stopPropagation();
                     open = false;
                     el.className = el.className.replace(/\bdr-menu-open\b/,'');
@@ -21,10 +52,4 @@ var YTMenu = (function() {
                 }
             }, false );
 
-        } );
- 
     }
- 
-    init();
- 
-})();
