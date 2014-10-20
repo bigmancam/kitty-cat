79276af2b35e02ade8afe4136b91ccdb50fe0fe4
Ladorne <xantos129@gmail.com>
2014-10-20 18:41:17 -0400

    js

--- django_project/django_project/static/js/menu.js
+++ django_project/django_project/static/js/menu.js
@@ -1,46 +1,8 @@
- $(document).ready(function() {
-  // initial setup
-  setup();
-  document.getElementById('content_pane').removeAttribute('hidden');
-});
-
-var meta;
-var transition;
-var state = {
-  opened: false
-}
-
-function getMeta() {
-      if (!meta) {
-        meta = document.createElement('core-meta');
-        meta.type = 'transition';
-      }
-      return meta;
-    }
-
-function setup() {
-      var target = document.getElementById('content_pane')
-
-      if (transition) {
-        transition.teardown(target);
-      }
-
-
-      var value = "core-transition-center";
-      transition = getMeta().byId(value);
-      transition.setup(target);
-    }
-
-function toggle1() {
-      var target = document.getElementById('content_pane')
-      state.opened = !state.opened;
-      transition.go(target, state);
-    }
-
-document.addEventListener('polymer-ready', function() {
+ document.addEventListener('polymer-ready', function() {
   // initial setup
   setup();
   document.getElementById('side').removeAttribute('hidden');
+  document.getElementById('content_pane').removeAttribute('hidden');
 });
 
 var meta;
@@ -69,6 +31,11 @@ function setup() {
       transition = getMeta().byId(value);
       transition.setup(target);
     }
+function toggle1() {
+      var target = document.getElementById('content_pane');
+      state.opened = !state.opened;
+      transition = getMeta().byId('core-transition-center');
+      transition.go(target, state);
 
 function toggle2() {
       var target = document.getElementById('side')
