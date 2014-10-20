c9381a3e910c672882de441a0290d6fdd4fad3e1
Ladorne <xantos129@gmail.com>
2014-10-20 17:23:13 -0400

    js

--- django_project/django_project/static/js/menu.js
+++ django_project/django_project/static/js/menu.js
@@ -4,17 +4,20 @@ var YTMenu = (function() {
          
         [].slice.call( document.querySelectorAll( '.dr-menu' ) ).forEach( function( el, i ) {
  
-            var trigger = document.querySelector( 'div.dr-trigger' );
-            var icon = document.querySelector( 'span.dr-icon-menu' );
-            var open = false;
+            var trigger = document.querySelector( 'div.dr-trigger' ),
+                icon = document.querySelector( 'span.dr-icon-menu' ),
+                open = false;
  
-            icon.addEventListener( 'click', function( event ) {
+            trigger.addEventListener( 'click', function( event ) {
                 if( !open ) {
                     el.className += ' dr-menu-open';
                     trigger.className += ' dr-menu-open';
                     icon.className += ' dr-menu-open';
                     open = true;
                 }
+            }, false );
+
+            icon.addEventListener( 'click', function( event ) {
                 if( open ) {
                     event.stopPropagation();
                     open = false;
@@ -32,4 +35,3 @@ var YTMenu = (function() {
     init();
  
 })();
-
