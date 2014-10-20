3b3c6c80373f9cafda557d87318b143be871d8c6
Cameron Fisk <cfisk@morristechnology.com>
2014-10-20 11:09:51 -0400

    css

--- django_project/django_project/static/js/menu.js
+++ django_project/django_project/static/js/menu.js
@@ -33,13 +33,8 @@ var YTMenu = (function() {
 })();
 
 document.addEventListener('polymer-ready', function() {
-  console.log("1" + document.querySelector('nav div.paper-shadow-top-z-1'));
-  console.log("2" + document.querySelector('paper-shadow paper-shadow-top-z-1'));
-  var a = document.getElementsByClassName('paper-shadow-top-z-1');
+  var a = document.querySelectorAll( 'div.paper-shadow-top-z-1' )
   console.log(a);
-  for(var i = 0; i < a.length; i++) {
-      console.log("3" + a[i]);
-  }
   // initial setup
   setup();
   document.querySelector('div.paper-shadow-top-z-1').removeAttribute('hidden');
