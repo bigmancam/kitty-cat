03b877b66b8d315ae0f2fc016fc99d7b385163cf
Ladorne <xantos129@gmail.com>
2014-10-21 16:29:38 -0400

    material

--- django_project/django_project/static/js/mainframe.js
+++ django_project/django_project/static/js/mainframe.js
@@ -42,21 +42,46 @@ function toggle() {
 }
 
 function toggleOff() {
-    var target = document.getElementById('timer');
+    var target = document.getElementById('countdown');
     state.opened = false;
     transition.go(target, state);
 }
 
 
 function submit(e){
+    var input = $('#fisk-input').context.activeElement.value.split(' ');
+    var arg = input[1];
     if(e.keyCode == 13) {
-        if($('#fisk-input').context.activeElement.value == 'timer') {
+        if(input[0] == 'timer') {
             toggle();
+            clock(arg);
             clearConsole();
         }
-        if($('#fisk-input').context.activeElement.value =='exit timer') {
+        if(input[0] + arg =='exit timer') {
             toggleOff();
             clearConsole();
         }
     }
+}
+
+function clock(s) {
+    var time_in_seconds = s * 60;
+    var seconds = time_in_seconds;
+
+    function secondPassed() {
+        var minutes = Math.round((seconds - 30) / 60);
+        var remainingSeconds = seconds % 60;
+        if (remainingSeconds < 10) {
+            remainingSeconds = "0" + remainingSeconds;
+        }
+        document.getElementById('clock').innerHTML = minutes + ":" + remainingSeconds;
+        if (seconds == 0) {
+            clearInterval(countdownTimer);
+            document.getElementById('clock').innerHTML = "Buzz Buzz";
+        } else {
+            seconds--;
+        }
+    }
+
+    var countdownTimer = setInterval('secondPassed()', 1000);
 }
