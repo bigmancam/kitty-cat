f5b279b1d7f42a3490fdd666a115f48eb829f12a
Ladorne <xantos129@gmail.com>
2014-10-21 16:37:51 -0400

    material

--- django_project/django_project/static/js/mainframe.js
+++ django_project/django_project/static/js/mainframe.js
@@ -68,17 +68,20 @@ function clock(s) {
     var time_in_seconds = s * 60;
     var seconds = time_in_seconds;
 
-    var minutes = Math.round((seconds - 30) / 60);
-    var remainingSeconds = seconds % 60;
-    if (remainingSeconds < 10) {
-        remainingSeconds = "0" + remainingSeconds;
+    function secondPassed() {
+        var minutes = Math.round((seconds - 30) / 60);
+        var remainingSeconds = seconds % 60;
+        if (remainingSeconds < 10) {
+            remainingSeconds = "0" + remainingSeconds;
+        }
+        document.getElementById('clock').innerHTML = minutes + ":" + remainingSeconds;
+        if (seconds == 0) {
+            clearInterval(countdownTimer);
+            document.getElementById('clock').innerHTML = "Buzz Buzz";
+        } else {
+            seconds--;
+        }
     }
-    document.getElementById('clock').innerHTML = minutes + ":" + remainingSeconds;
-    if (seconds == 0) {
-        clearInterval(countdownTimer);
-        document.getElementById('clock').innerHTML = "Buzz Buzz";
-    } else {
-        seconds--;
-    }
+
     var countdownTimer = setInterval('secondPassed()', 1000);
 }
